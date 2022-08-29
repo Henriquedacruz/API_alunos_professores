b5608272398a9687e674c0a264565c75104706a3
Antonio Henrique <88168605+Henriquedacruz@users.noreply.github.com>
2022-08-29 10:45:58 -0300

    deleteFrontTeste

--- FrontTeste/src/pages/Erro/index.js
+++
@@ -1,8 +0,0 @@
-import'./style.css'
-
-function index()  {
-  return (
-    <h1 className = 'ErroText' >PAGE NOT FOUND</h1>
-  ); 
-}
-export default index
--- FrontTeste/src/service/api.js
+++
@@ -1,8 +0,0 @@
-// yarn add axios
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL:'http://localhost:3333'
-})
-
-export default api;
